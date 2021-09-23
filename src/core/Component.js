9d27c0c3440c80e4a4f82730287abe5dec2be1c4
ChanYangYu <goodtss@soongsil.ac.kr>
2021-09-23 19:05:20 +0900

    feat : SPA Router 구현
    - Router Object Literal로 구현
    - 디렉토리 구조화
    - 간단한 페이지 이동 로직 추가

---
+++ src/core/Component.js
@@ -0,0 +1,23 @@
+class Component{
+    constructor(element, props){
+        this.element = element;
+        this.props = props;
+        this.setup();
+        this.render();
+        this.setEvent();
+    }
+    setup() {}
+    setEvent() {}
+    setState() {
+        this.render();
+        this.setEvent();
+    }
+    template() { return ''; }
+    mount(){}
+    render() {
+        this.element.innerHTML = this.template();
+        this.mount();
+    }
+}
+
+export default Component;
